da11cf9a9c40c20bef0e29971f06324a31c43f9d
vingurzhou <zwz0123460218@icloud.com>
2022-08-22 17:54:16 +0800

    init

--- leetcode/go/5. 最长回文子串/main.go
+++ leetcode/go/5. 最长回文子串/main.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 func longestPalindrome(s string) string {
+	length := 0
+	var result string
 	for i := range s {
 		for j := range s[i:] {
 			subString := []rune(s[i : i+j+1])
+			subString2 := make([]rune, len(subString))
+			copy(subString2, subString)
 			for l, r := 0, len(subString); l < len(subString)/2; l, r = l+1, r-1 {
 				subString[l], subString[r-1] = subString[r-1], subString[l]
 			}
-			fmt.Println(subString, subString)
+			if string(subString) == string(subString2) {
+				if len(subString) > length {
+					length = len(subString)
+					result = string(subString)
+				}
+			}
 		}
 	}
-	return ""
+	return result
 }
 
 func main() {
-	longestPalindrome("babad")
+	fmt.Println(longestPalindrome("babad"))
 }
