0aba469b8b10d592b9508819fed458d8cdc7f08a
vingurzhou <zwz0123460218@icloud.com>
2022-09-05 17:12:47 +0800

    init

--- structure/cirular_queue/main.go
+++ structure/cirular_queue/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import "fmt"
 
+type CirularQueue struct {
+	array [4]int
+	head  int
+	tail  int
+}
+
+func (this *CirularQueue) Push(val int) {
+	// fmt.Println((this.tail+1)%(len(this.array)+1), this.head)
+	if (this.tail+1)%(len(this.array)+1) == this.head {
+		panic("full")
+	}
+	this.array[this.tail] = val
+	this.tail++
+}
+func (this *CirularQueue) Pop() {
+	if this.head == this.tail {
+		panic("empty")
+	}
+	fmt.Println(this.array[this.head])
+	this.head++
+}
+func main() {
+	cq := &CirularQueue{array: [4]int{}, head: 0, tail: 0}
+	fmt.Println(cq)
+	cq.Push(1)
+	cq.Push(2)
+	cq.Push(3)
+	cq.Push(4)
+	fmt.Println(cq)
+	cq.Pop()
+	cq.Pop()
+	cq.Pop()
+	cq.Pop()
+	fmt.Println(cq)
 }
--- structure/queue/main.go
+++ structure/queue/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Queue struct {
+	array [4]int
+	front int
+	rear  int
+}
+
+func (this *Queue) Add(val int) {
+	if this.rear == len(this.array)-1 {
+		panic("queue full")
+	}
+	this.rear++
+	this.array[this.rear] = val
+	return
+}
+func (this *Queue) Get() {
+	if this.rear == this.front {
+		panic("queue empty")
+	}
+	this.front++
+	fmt.Println(this.array[this.front])
+	return
+}
+
 func main() {
+	queue := Queue{array: [4]int{},
+		front: -1, rear: -1}
+	fmt.Println(queue)
+	queue.Add(1)
+	queue.Add(2)
+	queue.Add(3)
+	queue.Add(4)
+	fmt.Println(queue)
+	queue.Get()
+	fmt.Println(queue)
+	queue.Get()
+	fmt.Println(queue)
 
 }
