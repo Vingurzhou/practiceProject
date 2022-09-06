d478150648e54cfb90e4af570559333bc2d7f2f9
vingurzhou <zwz0123460218@icloud.com>
2022-09-06 16:54:19 +0800

    init

--- structure/go/double_linked_list/main.go
+++ structure/go/double_linked_list/main.go
@@ -4,18 +4,21 @@ import "fmt"
 
 type ListNode struct {
 	val  int
+	pre  *ListNode
 	next *ListNode
 }
 
 func (this *ListNode) Insert(ln *ListNode) {
 	for this.next != nil {
 		this = this.next
+
 	}
 	this.next = ln
+	ln.pre = this
 }
 func (this *ListNode) Show() {
 	for this.next != nil {
-		fmt.Print(this.val, "\t")
+		fmt.Print(this, "\t")
 		this = this.next
 	}
 	if this.next == nil {
@@ -25,8 +28,9 @@ func (this *ListNode) Show() {
 }
 func (this *ListNode) Delete(val int) {
 	for this.next != nil {
-		if this.next.val == val {
-			this.next = this.next.next
+		if this.val == val {
+			this.next.pre = this.pre
+			this.pre.next = this.next
 		}
 		this = this.next
 	}
